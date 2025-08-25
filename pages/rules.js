01ec49f9fdbdfc8c5ec1ee003417a3c7cf49e096
mirzamagomedovg-blip <mirzamagomedovg@gmail.com>
2025-08-25 12:38:52 +0300

    Create rules.js

---
+++ pages/rules.js
@@ -0,0 +1,12 @@
+export default function Rules() {
+  return (
+    <div className="p-8 bg-black bg-opacity-60 min-h-screen">
+      <h1 className="text-3xl font-bold mb-6">
+        Это правила Winterfell убедительная просьба не нарушать
+      </h1>
+      <div id="rules-content">
+        <p>⚔️ Здесь будут правила (редактируются через админку).</p>
+      </div>
+    </div>
+  );
+}
