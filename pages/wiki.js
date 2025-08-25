6f8b96bd1249386b66d356e08a3816adf538adb4
mirzamagomedovg-blip <mirzamagomedovg@gmail.com>
2025-08-25 12:41:44 +0300

    Create wiki.js

---
+++ pages/wiki.js
@@ -0,0 +1,10 @@
+export default function Wiki() {
+  return (
+    <div className="p-8 bg-black bg-opacity-60 min-h-screen">
+      <h1 className="text-3xl font-bold mb-6">Википедия Winterfell</h1>
+      <div id="wiki-content">
+        <p>📖 Здесь будет информация, фото и тексты (редактируются через админку).</p>
+      </div>
+    </div>
+  );
+}
