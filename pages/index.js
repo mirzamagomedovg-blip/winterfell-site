b44abdb72b11eb6e1a47d34f76165d69c42959ac
mirzamagomedovg-blip <mirzamagomedovg@gmail.com>
2025-08-25 12:37:45 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-black bg-opacity-40">
+      <h1 className="text-5xl md:text-6xl font-bold">Добро пожаловать на Winterfell</h1>
+      <p className="mt-4 text-lg text-blue-200">Снежный город Minecraft ждёт вас ❄️</p>
+    </div>
+  );
+}
