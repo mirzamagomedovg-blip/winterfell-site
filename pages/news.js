9878c287725a98345dcc5332b78f31ea93f23670
mirzamagomedovg-blip <mirzamagomedovg@gmail.com>
2025-08-25 12:39:45 +0300

    Create news.js

---
+++ pages/news.js
@@ -0,0 +1,10 @@
+export default function News() {
+  return (
+    <div className="p-8 bg-black bg-opacity-60 min-h-screen">
+      <h1 className="text-3xl font-bold mb-6">Новости Winterfell</h1>
+      <div id="news-content">
+        <p>📰 Здесь будут фото и текстовые новости (редактируются через админку).</p>
+      </div>
+    </div>
+  );
+}
